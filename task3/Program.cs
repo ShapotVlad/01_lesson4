0abaa4c9f3517affc089967721a530009748d91b
Vladimir <vlnshapat@gmail.com>
2024-05-16 19:44:02 +0300

    add task3

---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// задача 3 ввести символы из клавиатуры и оставить только буквы
+
+
+string GetLetter(string s)
+{
+    string letter = "";
+    foreach (char e in s)
+    {
+        if (char.IsAsciiLetter(e) == true)
+        {
+            letter = letter + e;
+        }
+
+    }
+    return letter;
+}
+string str = Console.ReadLine();
+string result = GetLetter(str);
+Console.WriteLine(result);
