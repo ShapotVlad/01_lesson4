cea69214db1afb23a0aa2f30edd329ed2e308616
VladShapot <vlnshapot@gmail.com>
2024-05-17 15:21:18 +0300

    добавили изменения

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,7 +1,6 @@
 ﻿//создание и вывод на консоль  двухмерного массива [3, 5];
 
-int[,] metrix = new int[3, 5];
-
+int[,] metrix = new int[3, 4];           // создание случайного двумерного массива без исползования функции
 Random rnd = new Random();
 for (int i = 0; i < metrix.GetLength(0); i++)
 {
@@ -12,17 +11,17 @@ for (int i = 0; i < metrix.GetLength(0); i++)
 
 }
 
-for (int i = 0; i < metrix.GetLength(0); i++)
+for (int i = 0; i < metrix.GetLength(0); i++)  //вывод на консоль  без исползования функции
 {
     for (int j = 0; j < metrix.GetLength(1); j++)
     {
-        Console.Write($"{metrix[i, j]} ");
+        Console.Write($"{metrix[i, j],2}  ");
     }
-    Console.WriteLine();
+    Console.WriteLine();   //отделить строкой
 }
+Console.WriteLine();
 
-
-int[,] GreatMatrix(int strokcount, int stolbcount)
+int[,] GreatMatrix(int strokcount, int stolbcount) // создание случайного массива с исползованием функции
 {
     int[,] metrix = new int[strokcount, stolbcount];
 
@@ -31,23 +30,23 @@ int[,] GreatMatrix(int strokcount, int stolbcount)
     {
         for (int j = 0; j < metrix.GetLength(1); j++)
         {
-            metrix[i, j] = rnd.Next(1, 11);
+            metrix[i, j] = rnd.Next(1, 100);
         }
 
     }
     return metrix;
 }
-void ShowMatrix(int[,] matrix)
+void ShowMatrix(int[,] matrix)                //вывод на консоль с исползованием функции
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            Console.Write($"{matrix[i, j]} ");
+            Console.Write($"{matrix[i, j],4}");
         }
         Console.WriteLine();
     }
 }
 
-int[,] matrix = GreatMatrix(5, 4);
-ShowMatrix(matrix);
+int[,] matrix = GreatMatrix(4, 4);    //вызов функции создания массива
+ShowMatrix(matrix);                   //вызов функции  вывода массива    
