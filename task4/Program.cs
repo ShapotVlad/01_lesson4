fbfbc2f963c39ff988c350a214ccb849abe6a947
VladShapot <vlnshapot@gmail.com>
2024-05-17 13:16:43 +0300

    add task4

---
+++ task4/Program.cs
@@ -0,0 +1,16 @@
+﻿//выдать сообщение на экран
+//если в массиве есть нечетное число
+
+Console.WriteLine("start");
+
+int[] numbers = { 4, 8, 12, 22, 16, 13, 18 };
+
+foreach (int e in numbers)
+{
+    if( e % 2 == 1)
+    {
+        Console.WriteLine("есть число нечетное");
+        break;
+    }
+}
+Console.WriteLine("stop");
