2f6763090fe50177e981b8f7d889ab8a2dfe1442
VladShapot <vlnshapot@gmail.com>
2024-05-17 14:16:25 +0300

    add task6

---
+++ task6/Program.cs
@@ -0,0 +1,13 @@
+﻿// оператор управления continue
+
+for (int i = 1; i <=  5; i++)
+{
+    Console.Write($"итерация {i}: ");
+
+    if (i == 3)
+    {
+        Console.WriteLine("пропустить");
+        continue;
+    }
+    Console.WriteLine("некоторая обработка");
+}
