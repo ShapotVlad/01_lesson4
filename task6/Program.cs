68c39c64e3d0a5d4776ea1facb8c42d7715c4048
VladShapot <vlnshapot@gmail.com>
2024-06-03 13:22:35 +0300

    добавили изменения

--- task6/Program.cs
+++ task6/Program.cs
@@ -1,10 +1,10 @@
 ﻿// оператор управления continue
 
-for (int i = 1; i <=  5; i++)
+for (int i = 1; i <=  5; i++)//выводим  от 1 до 5
 {
     Console.Write($"итерация {i}: ");
 
-    if (i == 3)
+    if (i == 3)// пропустить 3 цикл
     {
         Console.WriteLine("пропустить");
         continue;
